ea87547d9f1728869f60a885349aa456f19cc3e1
ABojo <alex.bojorquez2598@gmail.com>
2021-05-18 22:34:34 -0400

    Created functions for getting a singular post and editing posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,6 +11,18 @@ exports.getAllPosts = async (req, res) => {
   });
 };
 
+exports.getPost = async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id).select('-_id -__v');
+
+  res.json({
+    status: 'success',
+    data: {
+      post,
+    },
+  });
+};
+
 exports.createPost = async (req, res) => {
   const { title, body, published } = req.body;
   const post = await Post.create({
@@ -19,6 +31,21 @@ exports.createPost = async (req, res) => {
     published,
     author: 'req.user.username',
   });
+
+  res.json({
+    status: 'success',
+    data: { post },
+  });
+};
+
+exports.editPost = async (req, res) => {
+  const { title, body, published } = req.body;
+  const { id } = req.params;
+  const post = await Post.findOneAndUpdate(
+    { _id: id },
+    { title, body, published },
+    { runValidators: true, new: true }
+  ).select('-_id -__v');
 
   res.json({
     status: 'success',
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const postController = require('../controllers/postController');
 
 router.get('/', postController.getAllPosts);
+router.get('/:id', postController.getPost);
 router.post('/', postController.createPost);
+router.put('/:id', postController.editPost);
 
 module.exports = router;
