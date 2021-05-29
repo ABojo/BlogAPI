2980b6b6ee67a53abbea2b67479188fc15d7b5c9
ABojo <alex.bojorquez2598@gmail.com>
2021-05-28 22:47:40 -0400

    Created a auth controller function to verify that the logged in users is the author of the post before allowing them to edit it

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,11 +2,22 @@ const User = require('../models/User');
 const catchError = require('../utils/catchError');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Post = require('../models/Post');
 
 exports.protect = catchError(async (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1];
   const user = await jwt.verify(token, process.env.JWT_SECRET);
 
   req.currentUser = user;
+  next();
+});
+
+exports.authorizeModify = catchError(async (req, res, next) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+
+  if (post.author !== req.currentUser.username)
+    throw new Error('You are not authroized to modify this post!');
+
   next();
 });
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,5 +6,6 @@ router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPost);
 router.post('/', authController.protect, postController.createPost);
 router.put('/:id', authController.protect, postController.editPost);
+router.delete('/:id', authController.protect, authController.authorizeModify);
 
 module.exports = router;
