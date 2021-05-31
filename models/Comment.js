3b6ff35f477d45ac995579eaede23a296f07dce9
ABojo <alex.bojorquez2598@gmail.com>
2021-05-31 02:35:28 -0400

    Added the post field to the comment model so its easier to check to make sure that the logged in user is authorized to delete the comment

--- models/Comment.js
+++ models/Comment.js
@@ -4,6 +4,7 @@ const commentSchema = new Schema({
   name: { type: String, required: true },
   body: { type: String, required: true },
   timestamp: { type: Date, required: true, default: Date.now },
+  post: { type: Schema.Types.ObjectId, ref: 'Post' },
 });
 
 const Comment = model('Comment', commentSchema);
