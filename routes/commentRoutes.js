04b9905dcb6b762a59a4585560905901fb62cb68
ABojo <alex.bojorquez2598@gmail.com>
2021-05-31 02:42:36 -0400

    Created the router for deleting comments and put the authenticaton and authorization middlewares at the front of the stack

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const commentController = require('../controllers/commentController');
+const authController = require('../controllers/authController');
 
 router.post('/', commentController.createComment);
+router.delete(
+  '/:id',
+  authController.protect,
+  authController.authorizeCommentModify,
+  commentController.deleteComment
+);
 
 module.exports = router;
